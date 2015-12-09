affe42f7738705a45009d1844fd6e78245b1e402
Sujan <najussujan@gmail.com>
2015-12-08 21:33:31 -0600

    Reducer class for Stripes

--- src/main/java/edu/mum/crystalball/stripes/StripesDriver.java
+++ src/main/java/edu/mum/crystalball/stripes/StripesDriver.java
@@ -1,9 +1,8 @@
 package edu.mum.crystalball.stripes;
 
-import java.util.HashMap;
-
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -11,12 +10,10 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-import edu.mum.wordcount.WordCount;
-
 public class StripesDriver extends Configured implements Tool {
 
 	public static void main(String[] args) throws Exception {
-		int exitCode = ToolRunner.run(new WordCount(), args);
+		int exitCode = ToolRunner.run(new StripesDriver(), args);
 		System.exit(exitCode);
 	}
 
@@ -27,23 +24,23 @@ public class StripesDriver extends Configured implements Tool {
 			ToolRunner.printGenericCommandUsage(System.err);
 			return -1;
 		}
-		
+
 		Job job = new Job();
 		job.setJarByClass(StripesDriver.class);
 		job.setJobName("StripesApproach");
-		
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
+
 		job.setMapperClass(StripesMapper.class);
 		job.setReducerClass(StripesReducer.class);
-		
+
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(HashMap.class);
-		
+		job.setMapOutputValueClass(MapWritable.class);
+
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(HashMap.class);
-		
+		job.setOutputValueClass(Text.class);
+
 		int returnValue = job.waitForCompletion(true) ? 0 : 1;
 		System.out.println("job.isSuccessful " + job.isSuccessful());
 		return returnValue;
--- src/main/java/edu/mum/crystalball/stripes/StripesReducer.java
+++ src/main/java/edu/mum/crystalball/stripes/StripesReducer.java
@@ -1,17 +1,61 @@
 package edu.mum.crystalball.stripes;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.text.DecimalFormat;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class StripesReducer extends Reducer<Text, HashMap<Text, IntWritable>, Text, HashMap<Text, IntWritable>> {
+public class StripesReducer extends Reducer<Text, MapWritable, Text, Text> {
+	MapWritable sumOfAllStripes = new MapWritable();
 
 	@Override
-	protected void reduce(Text key, Iterable<HashMap<Text, IntWritable>> value, Context context)
+	protected void reduce(Text key, Iterable<MapWritable> values, Context context)
 			throws IOException, InterruptedException {
+		sumOfAllStripes.clear();
+
+		for (MapWritable value : values) {
+			addStripeToFinalStripe(value);
+		}
+
+		int totalCount = 0;
+
+		for (Entry<Writable, Writable> entry : sumOfAllStripes.entrySet()) {
+			totalCount += ((IntWritable) entry.getValue()).get();
+		}
+		DecimalFormat decimalFormat = new DecimalFormat("0.000");
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (Entry<Writable, Writable> entry : sumOfAllStripes.entrySet()) {
+			if (stringBuilder.length() > 0)
+				stringBuilder.append(",");
+			stringBuilder.append("(").append(entry.getKey().toString()).append(",")
+					.append(decimalFormat.format(Integer.parseInt(entry.getValue().toString()) / (double) totalCount))
+					.append(") ");
+		}
+
+		context.write(key, new Text("[ " + stringBuilder.toString() + "] "));
 
 	}
+
+	private void addStripeToFinalStripe(MapWritable value) {
+
+		Set<Writable> allKeys = value.keySet();
+		for (Writable key : allKeys) {
+			IntWritable keyCountInCurrentStripe = (IntWritable) value.get(key);
+			if (sumOfAllStripes.containsKey(key)) {
+				IntWritable keyCountInResultantStripe = (IntWritable) sumOfAllStripes.get(key);
+				keyCountInResultantStripe.set(keyCountInCurrentStripe.get() + keyCountInResultantStripe.get());
+			} else {
+				sumOfAllStripes.put(key, keyCountInCurrentStripe);
+			}
+		}
+
+	}
+
 }
